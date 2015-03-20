aa32c3553167991cc0a436a8d272320353ab086d
Diana Ignacio <diana.ignacio@student.csulb.edu>
2015-03-20 13:56:35 -0700

    Added custom map providers and proxy settings

--- DianaRevision 3-12/Drone_Network/Drone_Gui/MainWindow.cs
+++ DianaRevision 3-12/Drone_Network/Drone_Gui/MainWindow.cs
@@ -11,7 +11,10 @@ using System.Windows.Input;
 using GMap.NET.MapProviders;
 using GMap.NET;
 using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms.ToolTips;
 using MissionPlanner.Controls;
+using MissionPlanner.Utilities;
 
 namespace Drone_Gui
 {
@@ -19,24 +22,43 @@ namespace Drone_Gui
     public partial class MainWindow : Form
     {
         //constructor and load methods
+         // layers
+              readonly GMapOverlay top = new GMapOverlay();
+              internal readonly GMapOverlay objects = new GMapOverlay("objects");
+              internal readonly GMapOverlay routes = new GMapOverlay("routes");
+              internal readonly GMapOverlay polygons = new GMapOverlay("polygons");
 
+              // marker
+              GMapMarker currentMarker;
+
+              // polygons
+              GMapPolygon polygon;
+
+              // etc
+              readonly Random rnd = new Random();
+              readonly DescendingComparer ComparerIpStatus = new DescendingComparer();
+              GMapMarkerRect CurentRectMarker = null;
+              string mobileGpsLog = string.Empty;
+              bool isMouseDown = false;
+              PointLatLng start;
+              PointLatLng end;
         
         public MainWindow()
         {
             InitializeComponent();
-
+             
             try
-{
-   System.Net.IPHostEntry e =
-        System.Net.Dns.GetHostEntry("www.google.com");
-}
-catch
-{
-   MainMap.Manager.Mode = AccessMode.CacheOnly;
-   MessageBox.Show("No internet connection avaible, going to CacheOnly mode.", 
-         "GMap.NET - Demo.WindowsForms", MessageBoxButtons.OK,
-         MessageBoxIcon.Warning);
-}
+            {
+               System.Net.IPHostEntry e =
+                    System.Net.Dns.GetHostEntry("www.google.com");
+            }
+            catch
+            {
+               MainMap.Manager.Mode = AccessMode.CacheOnly;
+               MessageBox.Show("No internet connection available, going to CacheOnly mode.", 
+                     "GMap.NET - Demo.WindowsForms", MessageBoxButtons.OK,
+                     MessageBoxIcon.Warning);
+            }
 
 // config map
 MainMap.MapProvider = GMapProviders.OpenStreetMap;
@@ -46,13 +68,41 @@ MainMap.MaxZoom = 24;
 MainMap.Zoom = 9;
 
 // add your custom map db provider
-//GMap.NET.CacheProviders.MySQLPureImageCache ch = new GMap.NET.CacheProviders.MySQLPureImageCache();
-//ch.ConnectionString = @"server=sql2008;User Id=trolis;Persist Security Info=True;database=gmapnetcache;password=trolis;";
-//MainMap.Manager.SecondaryCache = ch;
+            GMap.NET.MapProviders.GMapProviders.List.Add(Maps.WMSProvider.Instance);
+            GMap.NET.MapProviders.GMapProviders.List.Add(Maps.Custom.Instance);
+            GMap.NET.MapProviders.GMapProviders.List.Add(Maps.Earthbuilder.Instance);
+            GMap.NET.MapProviders.GMapProviders.List.Add(Maps.Statkart_Topo2.Instance);
+            GMap.NET.MapProviders.GMapProviders.List.Add(Maps.MapBox.Instance);
+            GMap.NET.MapProviders.GMapProviders.List.Add(Maps.MapboxNoFly.Instance);
 
 // set your proxy here if need
-//GMapProvider.WebProxy = new WebProxy("10.2.0.100", 8080);
-//GMapProvider.WebProxy.Credentials = new NetworkCredential("ogrenci@bilgeadam.com", "bilgeada");
+            GMap.NET.MapProviders.GMapProvider.WebProxy = WebRequest.GetSystemWebProxy();
+            GMap.NET.MapProviders.GMapProvider.WebProxy.Credentials = CredentialCache.DefaultCredentials;
+
+            WebRequest.DefaultWebProxy = WebRequest.GetSystemWebProxy();
+            WebRequest.DefaultWebProxy.Credentials = CredentialCache.DefaultNetworkCredentials;
+
+            string name = "Mission Planner";
+
+            if (File.Exists(Application.StartupPath + Path.DirectorySeparatorChar + "logo.txt"))
+                name = File.ReadAllText(Application.StartupPath + Path.DirectorySeparatorChar + "logo.txt", Encoding.UTF8);
+
+            if (File.Exists(Application.StartupPath + Path.DirectorySeparatorChar + "logo.png"))
+                Logo = new Bitmap(Application.StartupPath + Path.DirectorySeparatorChar + "logo.png");
+
+            if (name == "VVVVZ")
+            {
+                vvvvz = true;
+                // set pw
+                MainWindow.config["password"] = "viDQSk/lmA2qEE8GA7SIHqu0RG2hpkH973MPpYO87CI=";
+                MainWindow.config["password_protect"] = "True";
+                // prevent wizard
+                MainWindow.config["newuser"] = "11/02/2014";
+                // invalidate update url
+                System.Configuration.ConfigurationManager.AppSettings["UpdateLocationVersion"] = "";
+            }
+
+            CleanupFiles();
 
 // map events
 {
@@ -107,6 +157,61 @@ MainMap.Zoom = 9;
             main_view.Image = secondary_view.Image;
             secondary_view.Image = temp;
         }
+      void OnTileCacheProgress(int left)
+      {
+         if(!IsDisposed)
+         {
+            MethodInvoker m = delegate
+            {
+               textBoxCacheStatus.Text = left + " tile to save...";
+            };
+            Invoke(m);
+         }
+      }
+        static void CleanupFiles()
+        {
+            //cleanup bad file
+            string file = Application.StartupPath + Path.DirectorySeparatorChar + @"LogAnalyzer\tests\TestUnderpowered.py";
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
+            //File.Delete("*.xaml");
+        }
+    // loader start loading tiles
+      void MainMap_OnTileLoadStart()
+      {
+         MethodInvoker m = delegate()
+         {
+            panelMenu.Text = "Menu: loading tiles...";
+         };
+         try
+         {
+            BeginInvoke(m);
+         }
+         catch
+         {
+         }
+      }
 
+      // loader end loading tiles
+      void MainMap_OnTileLoadComplete(long ElapsedMilliseconds)
+      {
+         MainMap.ElapsedMilliseconds = ElapsedMilliseconds;
+
+         MethodInvoker m = delegate()
+         {
+            panelMenu.Text = "Menu, last load in " + MainMap.ElapsedMilliseconds + "ms";
+
+            textBoxMemory.Text = string.Format(CultureInfo.InvariantCulture, "{0:0.00} MB of {1:0.00} MB", MainMap.Manager.MemoryCache.Size, MainMap.Manager.MemoryCache.Capacity);
+         };
+         try
+         {
+            BeginInvoke(m);
+         }
+         catch
+         {
+         }
+      }
     }
-}
+
