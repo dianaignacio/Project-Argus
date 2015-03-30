9991c1dfa93cafb5aa92a632d8dab41ab272f228
Eli Blain <eliblain@gmail.com>
2015-03-30 10:54:01 -0700

    Moved beacon to it's own folder.

---
+++ Arduino/Beacon/Beacon_tested/Beacon_tested.ino
@@ -0,0 +1,139 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+#include <LiquidCrystal.h>
+#include <XBee.h>
+
+
+// create the XBee object, buffer, address, response packet, and transmit packet
+XBee xbee = XBee();  
+uint8_t buffer[20];
+char temp[20];
+XBeeAddress64 addr64 = XBeeAddress64(0,0);
+ZBTxRequest zbTx1 = ZBTxRequest(addr64,buffer,sizeof(buffer));
+ZBRxResponse zbRx = ZBRxResponse();
+
+// initialize the library with the numbers of the interface pins
+/*                Rs  En  D4  D5  D6 D7    */
+LiquidCrystal lcd(9, 8, 5, 4, 3, 2);
+
+/*Additionally,
+   10K pot is wired to +5V and GND, with it's wiper (output) to LCD screens VO pin (pin3).*/
+
+/*  9600-baud serial GPS device hooked up on pins 12(rx) and 13(tx). */
+
+TinyGPS gps;
+SoftwareSerial ss(12, 13);
+
+float flat, flon;
+    unsigned long age;
+
+void setup()
+{ 
+  Serial.begin(9600);
+  xbee.begin(Serial);
+   // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  lcd.setCursor(3, 0);lcd.print("Mobile Home");
+  lcd.setCursor(2, 1);lcd.print("Point Beacon");
+  // initialize the serial communications:
+  Serial.begin(9600);
+  ss.begin(9600);
+  
+  Serial.print("Mobile Home Point Beacon"); 
+  Serial.println();
+  Serial.println("by Elizabeth");
+  Serial.println();
+     delay(1000);
+     delay(1000); 
+     lcd.clear();
+}
+
+void loop()
+{
+ 
+  bool newData = false;
+
+  // For one second we parse GPS data and report some key values
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (ss.available())
+    {
+      char c = ss.read();
+      // Serial.write(c); // uncomment this line to see the GPS data flowing
+      if (gps.encode(c))  // Did a new valid sentence come in?
+        newData = true;
+    }
+  }
+
+  if (newData)
+  {
+    lcd.clear();
+    gps.f_get_position(&flat, &flon, &age);
+    //Serial.print("LAT-LON=");
+    //Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 4);
+      // Print a message to the LCD.
+       lcd.print("LAT: "); 
+         lcd.print(flat);
+    //Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 4);
+    //Serial.println();
+      lcd.setCursor(0,1);
+      lcd.print("LON: ");
+         lcd.print(flon);
+  }
+  
+  /*
+  // when characters arrive over the serial port...
+  if (Serial.available()) {
+    // wait a bit for the entire message to arrive
+    delay(100);
+    // clear the screen
+    lcd.clear();
+    // read all the available characters
+    while (Serial.available() > 0) {
+      // display each character to the LCD
+      lcd.write(Serial.read());
+    }
+  }
+  */
+  if (xbee.readPacket(500)) 
+  {
+    if(xbee.getResponse().getApiId() == ZB_RX_RESPONSE)
+    {
+      xbee.getResponse().getZBRxResponse(zbRx);
+/*
+      for(int i = 0;i<zbRx.getDataLength(); i++)
+      {
+        if(i<sizeof(buffer))
+          buffer[i] = zbRx.getData(i);
+      }
+*/    
+      
+      dtostrf(flat,4,4,temp);
+      for(int i = 0; i < 20; i++)
+      {
+       buffer[i] = temp[i]; 
+      }
+      /*
+      for(int i = 0; i < 20; i++)
+      {
+       Serial.print(buffer[i]); 
+      }
+      */
+      zbTx1 = ZBTxRequest(addr64, buffer, zbRx.getDataLength());
+      xbee.send(zbTx1);
+    }
+  } 
+  else if (xbee.getResponse().isError()) 
+  {
+    //nss.print("Error reading packet.  Error code: ");  
+    //nss.println(xbee.getResponse().getErrorCode());
+  } 
+  else {
+    // local XBee did not provide a timely TX Status Response -- should not happen
+    //flashLed(errorLed, 10, 50);
+  }
+  
+  
+}
+
+
