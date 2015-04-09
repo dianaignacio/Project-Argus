2e49eddc31fedf5576c0b59d528810ce0c99668d
Eli Blain <eliblain@gmail.com>
2015-04-09 15:34:28 -0700

    Simple map displaying with GMap. Contains no functionality yet.

---
+++ Maps-Eli/MapsTest/MapsTest/MapsTest/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using GMap.NET.CacheProviders;
+using GMap.NET.Internals;
+using GMap.NET.MapProviders;
+using GMap.NET.ObjectModel;
+using GMap.NET.Projections;
+using GMap.NET.Properties;
+using GMap.NET.WindowsForms;
+using System.Net;
+
+namespace MapsTest
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+
+            SuspendLayout();
+
+            /* To be used if there is a proxy on the network            
+            
+            GMapProvider.WebProxy = new WebProxy("10.2.0.100", 8080);
+            GMapProvider.WebProxy.Credentials = new NetworkCredential("ogrenci@bilgeadam.com", "bilgeada");
+            */
+            //used to initialize map control, overlay, and markers
+            GMapControl mapControl = new GMapControl();
+            {
+                mapControl.MapProvider = GMapProviders.BingMap;
+                mapControl.Position = new GMap.NET.PointLatLng(54.6961334816182, 25.2985095977783);
+                mapControl.MinZoom = 1;
+                mapControl.MaxZoom = 17;
+                mapControl.Zoom = 9;
+                mapControl.Dock = DockStyle.Fill;
+            }
+
+            Controls.Add(mapControl);
+            ResumeLayout(true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+    }
+}
