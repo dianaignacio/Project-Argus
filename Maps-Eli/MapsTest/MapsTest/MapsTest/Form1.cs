569814486e8a8029d069aad6b9b3ba0a195997ec
Eli Blain <eliblain@gmail.com>
2015-04-10 14:54:13 -0700

    Map can now be dragged by using holding right click. Standard map features are all functional.

--- Maps-Eli/MapsTest/MapsTest/MapsTest/Form1.cs
+++ Maps-Eli/MapsTest/MapsTest/MapsTest/Form1.cs
@@ -34,25 +34,30 @@ namespace MapsTest
             GMapProvider.WebProxy.Credentials = new NetworkCredential("ogrenci@bilgeadam.com", "bilgeada");
             */
             //used to initialize map control, overlay, and markers
-            GMapControl mapControl = new GMapControl();
-            {
-                mapControl.MapProvider = GMapProviders.BingHybridMap;
-                mapControl.Position = new GMap.NET.PointLatLng(54.6961334816182, 25.2985095977783);
-                mapControl.MinZoom = 1;
-                mapControl.MaxZoom = 20;
-                mapControl.Zoom = 9;
-                mapControl.Dock = DockStyle.Fill;
-            }
+            
 
             Controls.Add(mapControl);
             ResumeLayout(true);
 
-            GMapOverlay ov1 = new GMapOverlay("ov1");
-            mapControl.Overlays.Add(ov1);
+            GMapOverlay markerTest = new GMapOverlay("markers");
+            mapControl.Overlays.Add(markerTest);
+
+            //GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(-25.966688, 32.580528),GMarkerGoogleType.green);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
+
+        private void mapControl_Load(object sender, EventArgs e)
+        {
+            mapControl.MapProvider = GMapProviders.BingHybridMap;
+            mapControl.Position = new GMap.NET.PointLatLng(54.6961334816182, 25.2985095977783);
+            mapControl.MinZoom = 0;
+            mapControl.MaxZoom = 18;
+            mapControl.Zoom = 9;
+            mapControl.Dock = DockStyle.Fill;
             
         }
 
